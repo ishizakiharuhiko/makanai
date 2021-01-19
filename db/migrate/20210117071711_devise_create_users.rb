d13b931b5745722ecee689195ee01ec7ac65e105
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2021-01-20 00:09:18 +0900

    Revert "userテーブル追加"
    This reverts commit 1dc69e838a403e9b92cddc5f20a20c9510c8994b.

--- db/migrate/20210117071711_devise_create_users.rb
+++ db/migrate/20210117071711_devise_create_users.rb
@@ -4,20 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string  :email,              null: false, default: "", unique: true
-      t.string  :encrypted_password, null: false, default: ""
-
-      t.string  :name,               null: false
-      t.text    :profile,            null: false
-      t.integer :budget,             null: false
-      t.integer :category_id,        null: false
-      t.integer :prefecture_id,      null: false
-      t.string  :city,               null: false
-      t.string  :street,             null: false
-      t.string  :building
-      t.string  :phone_number,       null: false
-
-
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,6 @@ ActiveRecord::Schema.define(version: 2021_01_17_071711) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
-    t.text "profile", null: false
-    t.integer "budget", null: false
-    t.integer "category_id", null: false
-    t.integer "prefecture_id", null: false
-    t.string "city", null: false
-    t.string "street", null: false
-    t.string "building"
-    t.string "phone_number", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
