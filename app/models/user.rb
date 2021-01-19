218549fa286f3f856834b06144d4d1249acf7105
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2021-01-20 01:03:11 +0900

    userバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :recipes
+
+  with_options presence: true do
+    validates :name
+    validates :profile
+    validates :budget
+    validates :city
+    validates :street
+    validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :prefecture_id
+    end
+  end
 end
