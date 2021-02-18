b9f08b63ac840289b6b026c07cd29b012fc066f2
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2021-02-18 16:52:16 +0900

    rspec,Factorybot,fakerの導入

--- db/migrate/20210121023817_create_recipes.rb
+++ db/migrate/20210121023817_create_recipes.rb
@@ -2,11 +2,12 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
 
-      t.string  :title,       null: false
-      t.text    :material,    null: false
-      t.text    :making,      null: false
-      t.text    :point,       null: false
-      t.integer :category_id  null: false
+      t.string     :title,       null: false
+      t.text       :material,    null: false
+      t.text       :making,      null: false
+      t.text       :point,       null: false
+      t.integer    :category_id, null: false
+      t.references :user,        foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,11 @@ ActiveRecord::Schema.define(version: 2021_02_04_044854) do
     t.text "material", null: false
     t.text "making", null: false
     t.text "point", null: false
+    t.integer "category_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -64,4 +67,5 @@ ActiveRecord::Schema.define(version: 2021_02_04_044854) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "recipes", "users"
 end
